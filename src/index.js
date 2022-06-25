742dea405f1d733a3c7b8032614e032271f6c1b4
Ashe546 <asfyd42@gmail.com>
2022-06-25 12:02:00 -0300

    Implemented the leaderbords api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import _ from 'lodash';
 import './style.css';
+import show from './modules/show';
 
-function component() {
-  console.log(_);
+const submit = document.getElementById('submit');
+const refresh = document.getElementById('refresh');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lGcv4uHBWKY5C0XB2342/scores';
+
+submit.addEventListener('click', () => {
+  const user = document.getElementById('user');
+  const score = document.getElementById('score');
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: user.value.trim(),
+      score: score.value.trim(),
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  user.value = '';
+  score.value = '';
+});
+
+async function getScore() {
+  const requestURL = url;
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const scores = await response.json();
+  const { result } = scores;
+  document.querySelector('.leaderboard-score');
+  result.forEach((score) => {
+    show(score.user, score.score);
+  });
 }
 
-document.body.appendChild(component());
+refresh.addEventListener('click', getScore);
